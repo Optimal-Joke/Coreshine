ff2766cba6a4bcb629a124528b3a513b8a54eea5
Hunter Holland <hunterholland@Hunters-MacBook-Pro-15.local>
2020-05-20 17:57:58 -0400

    Consolidation into one Jupyter notebook that references other function files

---
+++ XMM_functions.py
@@ -0,0 +1,3 @@
+from astropy.io import fits
+import numpy as np
+import matplotlib.pyplot as plt
--- chandra_functions.py
+++ chandra_functions.py
@@ -3,15 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def make_hist(filepath, min_e, max_e, nbins='auto', i=None):
+def make_hist(obj_name, filepath, min_e, max_e, nbins='auto', i=None):
     evt_data = fits.getdata(filepath)
     energy = evt_data["energy"]
     min_thresh = evt_data["energy"] >= min_e
     max_thresh = evt_data["energy"] < max_e
     e_band = energy[min_thresh & max_thresh]
     plt.hist(e_band, bins=nbins)
-    plt.title("Test")
+    plt.title(
+        f"Energy Distribution for {obj_name} between {min_e} and {max_e} eV")
     plt.xlabel("Energy (eV)")
     plt.ylabel("Count")
-    plt.savefig(f"energy_hist_{min_e}_{max_e}_{i}", format="png")
+    plt.savefig(f"{obj_name}_ehist_{min_e}-{max_e}eV", dpi=250, format="png")
     plt.close()
